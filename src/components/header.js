7ab4f0dbad1b8b845a6002550d80ebb2329a2830
Jacob D. Castro <jdcastro.business@gmail.com>
2019-07-29 17:27:20 -0700

    add components for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,82 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useState } from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-// styled-components
-import Container from '../styles/header';
+// CSS-IN-JS styled-components
+import Wrapper from '../styles/header-styles';
+import '../utils/hamburgers.css';
 
-const Header = ({ siteTitle }) => (
-  <Container>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="#contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  </Container>
-);
+const Header = ({ headerData }) => {
+  let [mobileNavIsOpen, toggleMobileNav] = useState(false);
+  const data = useStaticQuery(graphql`
+    query HeaderLogoQuery {
+      contentfulAsset(id: { eq: "c5119d81-21d5-5d89-bf6d-761c701f0e9e" }) {
+        id
+        title
+        description
+        fluid(quality: 100) {
+          ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+    }
+  `);
+
+  return (
+    <Wrapper>
+      <Img
+        className="headerImage"
+        fluid={headerData.backgroundImage.fluid}
+        alt={headerData.description}
+      />
+
+      <div className="headerContainer">
+        <nav>
+          <Link to="/">
+            <Img
+              style={{
+                width: '35vw',
+                height: 'auto',
+              }}
+              className="headerLogo"
+              fluid={data.contentfulAsset.fluid}
+              alt={data.contentfulAsset.description}
+            />
+          </Link>
+
+          {/* <ul id="fullNav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul> */}
+
+          <button
+            id="hamburger"
+            className={`hamburger hamburger--spin${mobileNavIsOpen &&
+              ` is-active`}`}
+            onClick={() =>
+              toggleMobileNav((mobileNavIsOpen = !mobileNavIsOpen))
+            }
+            type="button"
+          >
+            <span class="hamburger-box">
+              <span class="hamburger-inner" />
+            </span>
+          </button>
+        </nav>
+
+        <div className="headerContentSection">
+          <h2>asdfjkl;</h2>
+          <a href="#footer">Contact us!</a>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 export default Header;
