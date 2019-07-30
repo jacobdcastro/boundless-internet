0180921d35c0e346065f6a659b5931c3cc6187ae
Jacob D. Castro <jdcastro.business@gmail.com>
2019-07-29 17:25:40 -0700

    some init stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/boundlesslogo.jpg`, // This path is relative to the root of the site.
       },
     },
     {
@@ -38,4 +38,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
