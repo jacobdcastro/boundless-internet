0b811e7cb4df6405f2ddce432d006d9da78c6eb2
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-05 14:53:11 -0700

    begin development

---
+++ src/components/MobileNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from '../styles/mobilenav-styles';
+
+const MobileNav = () => {
+  return (
+    <Wrapper>
+      <div>Umm hi</div>
+    </Wrapper>
+  );
+};
+
+export default MobileNav;
